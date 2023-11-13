8827019ecfab0448977d9227fed708cc55267372
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-14 00:14:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function toCase(text) {
     // write your code here
     //copy
+	let Un =text.toLowerCase();
     let Nu = text.toUpperCase();
-    return text+"-"+Nu;
-  }
+    return Un+"-"+Nu;
+  } 
   
   // DO not change the code below
   
-  const text = prompt("Enter text:");
-  alert(toCase(text));
+   const text = prompt("Enter text:");
+   alert(toCase(text));
